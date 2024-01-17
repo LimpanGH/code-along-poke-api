620d97a1ba5d03981e9fc0624db4124dce90969d
LimpanGH <Linusj555@gmail.com>
2024-01-17 14:17:53 +0100

    små korrigeringar utifrån sandras repo

--- vite-project/main.js
+++ vite-project/main.js
@@ -20,19 +20,18 @@ async function getPokemonList() {
     );
     // console.log(response.data.results);
     return response.data.results;
-  } catch {
+  } catch (err) {
     console.error('Error fectching pokemon list', err.message);
     return []; // returnerar en tom array om man inte får nåt response
   }
 }
-getPokemonList();
 
 async function getPokemonDetails(pokemonName) {
   try {
     const response = await axios.get(
       `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
     );
-    console.log(response.data);
+    // console.log(response.data);
     const data = response.data;
     // console.log(response.data.base_experience)
     // console.log(response.data.abilities[0].ability.name)
@@ -40,21 +39,51 @@ async function getPokemonDetails(pokemonName) {
 
     const abilities = data.abilities.map((item) => item.ability.name);
     const base_experience = data.base_experience;
-    console.log(abilities, base_experience);
+    // console.log(abilities, base_experience);
 
-    console.log(abilities);
+    return {
+      // abilities: abilities,
+      // base_experience: base_experience   -- samma sak som nedan
+      abilities,
+      base_experience,
+    };
+
+    // console.log(abilities);
   } catch (err) {
     console.error('Error fetching pokemon details', err.message);
+    return {
+      abilities: [],
+      base_experience: null,
+    };
   }
 }
 
 // Denna funktion ansvarar för att rendera ut pokemon-info i varje pokemonkort
-function renderPokemonCards(pokemonList) {
+async function renderPokemonCards(pokemonList) {
   // const data = await pokemonList
   // console.log(data);
-
   const cardsContainer = document.querySelector('.pokemon-cards-container');
-  cardsContainer.innerHTML = pokemonList
+
+  const completePokeonList = await Promise.all(
+    pokemonList.map(async (pokemon, index) => {
+      const { abilities, base_experience } = await getPokemonDetails(
+        pokemon.name
+      ); // destructering
+      return {
+        // samla komplett info om en pokemon(name, index, abilites, base_exp)
+        name: pokemon.name,
+        abilities: abilities,
+        base_experience: base_experience,
+        index: index,
+      };
+    })
+  );
+
+  console.log(completePokeonList);
+
+  //Filtrera,sortera...
+
+  cardsContainer.innerHTML = completePokeonList
     .map((pokemon, index) => {
       return `
   <article class="pokemon-card">
@@ -62,8 +91,8 @@ function renderPokemonCards(pokemonList) {
      index + 1
    }.png"> 
    <h2>${pokemon.name}</h2>
-   <p>Abilities: TODO</p>
-   <p>Base Experience: </p>
+   <p>Abilities: ${pokemon.abilities.join(', ')}</p> 
+   <p>Base Experience: ${pokemon.base_experience}</p>
    </article>
    `;
     })
@@ -74,8 +103,8 @@ async function main() {
   const pokemonList = await getPokemonList();
   // Köra function som renderar ut pokemon-card data.
   renderPokemonCards(pokemonList);
-  const data = getPokemonDetails('pikachu');
-  console.log('pokemon details', data);
+  const data = await getPokemonDetails('pikachu');
+  // console.log('pokemon details', data);
 }
 
 main();
